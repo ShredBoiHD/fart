7b15cc6da4dfc3d3323bc358122a70799a04e36f
ShredBoiHD <95959932+ShredBoiHD@users.noreply.github.com>
2022-10-09 20:54:34 +0200

    latest relics

--- example_mods/stages/dork.lua
+++ example_mods/stages/dork.lua
@@ -1,21 +1,21 @@
 function onCreate()
 	-- background shit
-	makeLuaSprite('gates', 'gates', -600, -300);
-	setLuaSpriteScrollFactor('stageback', 2, 2);
+	makeLuaSprite('DAVE', 'DAVE', -500, -300);
+	setLuaSpriteScrollFactor('DAVE', 0.9, 0.9);
 	
-	makeLuaSprite('grass', 'grass', -650, 600);
-	setLuaSpriteScrollFactor('grass', 2, 2);
-	scaleObject('grass', 2, 2);
+	makeLuaSprite('DAVE', 'DAVE', -650, 600);
+	setLuaSpriteScrollFactor('DAVE', 0.9, 0.9);
+	scaleObject('DAVE', 3, 3);
 
 	-- sprites that only load if Low Quality is turned off
 	if not lowQuality then
-		makeLuaSprite('hills', 'hills', -125, -100);
+		makeLuaSprite('no', 'no', -125, -100);
 		setLuaSpriteScrollFactor('no', 0.9, 0.9);
-		scaleObject('hills', 2, 2);
+		scaleObject('no', 1.1, 1.1);
 		
-		makeLuaSprite('sky', 'sky', 1225, -100);
+		makeLuaSprite('no', 'no', 1225, -100);
 		setLuaSpriteScrollFactor('no', 0.9, 0.9);
-		scaleObject('sky', 2.5, 2.8);
+		scaleObject('no', 1.1, 1.1);
 		setPropertyLuaSprite('no', 'flipX', true); --mirror sprite horizontally
 
 		makeLuaSprite('no', 'no', -500, -300);
@@ -23,10 +23,10 @@ function onCreate()
 		scaleObject('no', 0.9, 0.9);
 	end
 
-	addLuaSprite('gates', false);
-	addLuaSprite('grass', false);
-	addLuaSprite('hills', false);
-	addLuaSprite('sky', false);
+	addLuaSprite('DAVE', true);
+	addLuaSprite('DAVE', true);
+	addLuaSprite('no', false);
+	addLuaSprite('no', false);
 	addLuaSprite('no', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
